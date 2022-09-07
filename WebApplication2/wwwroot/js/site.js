8a0dae2bf7e9712c3a99947ebb62bedbcdbccf65
Salman <salmanshafiq00@gmail.com>
2022-09-07 07:50:36 +0600

    edit

--- WebApplication2/wwwroot/js/site.js
+++ WebApplication2/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("#chk_0").click(function () {
+        console.log("Hello");
+        $("#TestConfigDetails_0__SubjectId").removeAttr('disabled');
+        $("#TestConfigDetails_0__ResultGradeId").removeAttr('disabled');
+    })
+    $("#chk_1").click(function () {
+        console.log("Hello");
+        $("#TestConfigDetails_1__SubjectId").removeAttr('disabled');
+        $("#TestConfigDetails_1__ResultGradeId").removeAttr('disabled');
+    })
+    $("#chk_2").click(function () {
+        console.log("Hello");
+        $("#TestConfigDetails_2__SubjectId").removeAttr('disabled');
+        $("#TestConfigDetails_2__ResultGradeId").removeAttr('disabled');
+    })
+})
